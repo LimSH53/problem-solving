f7c61887915078dcc7a5c6ff2c6de13bd50dd800
Suhyeon Lim <tgtg5174@gmail.com>
2022-01-13 20:45:10 +0900

    자바 백준 레벨1 문제풀이 일부

---
+++ src/level01/num01.java
@@ -0,0 +1,13 @@
+package level01;
+
+public class num01 { // 제출할땐 Main으로..
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+
+	}
+
+}
